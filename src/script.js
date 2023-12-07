b3e23ffc935611d12985445e44cd90f3780e5519
gbshsc <ilkin.mengusoglu.23@ucl.ac.uk>
2023-12-07 00:35:07 +0000

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,20 @@
+(function() {
+    // Create overlay element
+    var overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
+
+    document.addEventListener('scroll', function() {
+        var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+        var maxScrollHeight = Math.max(
+            document.body.scrollHeight, document.body.offsetHeight,
+            document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight
+        ) - window.innerHeight;
+
+        // Calculate the scroll percentage
+        var scrollPercentage = (scrollPosition / maxScrollHeight * 0.5) ** 0.3;
+
+        // Set the opacity of the overlay based on the scroll percentage
+        overlay.style.opacity = scrollPercentage;
+    });
+})();
