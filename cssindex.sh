213222767666a0899dd51eb63b792743efdbb1e0
gbshsc <ilkin.mengusoglu.23@ucl.ac.uk>
2024-02-26 00:47:38 +0000

    icon sizes and zoom

---
+++ cssindex.sh
@@ -0,0 +1,7 @@
+fullfile=$(ls dist/assets/index*.css)
+filename="${fullfile##*/}"
+elem=$(sed -n 's/.*<link href="dist\/assets\/index-\(.*\).css" rel="stylesheet">.*/\1/p' index.html)
+for html in *html
+do
+eval "sed -i 's/index-${elem}.css/${filename}/g' $html"
+done
